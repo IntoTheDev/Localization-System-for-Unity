4f37a475117ac23177454a0e7ded79644ee5b1fa
Vladislav Kinyashov <53948684+IntoTheDev@users.noreply.github.com>
2019-11-03 12:48:34 +0500

    Update

---
+++ LocalizationTMP.cs
@@ -0,0 +1,13 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace ToolBox.Localization
+{
+	public class LocalizationTMP : MonoBehaviour
+	{
+		[SerializeField] private TMP_Text textComponent = null;
+		[SerializeField] private string localizationKey = "";
+
+		private void Start() => textComponent.text = Localization.LocalizeText(localizationKey);
+	}
+}
